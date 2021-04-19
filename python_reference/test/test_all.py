54635bd3d60be67a02edf4ef569c81738d663b2d
Simon <simon.juergensmeyer@gmail.com>
2021-04-19 16:48:00 +0200

    Remove reference code folder
    Otherwise, github will list the repository as a Python project,
    because there is more Python than Go code.

--- python_reference/test/test_all.py
+++
@@ -1,6 +0,0 @@
-from astral.geocoder import all_locations
-from astral.sun import noon
-
-def test_AllLocations(test_database):
-    for location in all_locations(test_database):
-        noon(location.observer)
--- python_reference/test/test_buenos_aries.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from astral.geocoder import lookup
-
-
-def test_BuenosAries(test_database):
-    b = lookup("Buenos Aires", test_database)
-    assert b.timezone == "America/Buenos_Aires"
